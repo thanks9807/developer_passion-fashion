0930833944d3641170d2b716b23ff099c780f0b5
thanks9807 <thanks9807@naver.com>
2024-02-22 17:18:09 +0900

    [test] test5

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -97,7 +97,7 @@
                 </div>
               </li>
               <li class="nav-item">
-                <a href="/product"> CATEGORYyyy </a>
+                <a href="/product"> CATEGORYyyxxxy </a>
                 <div class="nav-item-dropdown">
                   <div class="dropdown-menu">
                     <div class="dropdown-menu-header">
